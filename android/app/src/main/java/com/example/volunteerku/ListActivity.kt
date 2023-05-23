e802a2fa973833f5399e7d8ebbbe3daff3c5fcde
plan6062 <qwa7854@naver.com>
2023-05-24 02:19:53 +0900

    feat:initLayout

--- android/app/src/main/java/com/example/volunteerku/ListActivity.kt
+++ android/app/src/main/java/com/example/volunteerku/ListActivity.kt
@@ -1,11 +1,11 @@
 package com.example.volunteerku
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class ListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_list)
     }
 }
