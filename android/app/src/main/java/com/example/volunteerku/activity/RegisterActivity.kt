cae48fec0d2b1463517b44c0cf54d48d9a15225f
plan6062 <qwa7854@naver.com>
2023-05-29 10:30:09 +0900

    feat:add Btn listener

--- android/app/src/main/java/com/example/volunteerku/activity/RegisterActivity.kt
+++ android/app/src/main/java/com/example/volunteerku/activity/RegisterActivity.kt
@@ -6,23 +6,29 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.volunteerku.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
-    lateinit var binding:ActivityRegisterBinding
+    lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.nextButton.setOnClickListener{
+
+
+        binding.nextButton.setOnClickListener {
             val title = binding.titleEt.text.toString()
             val content = binding.contentEt.text.toString()
             val intent = Intent(this, Register2Activity::class.java)
             intent.putExtra("title", title)
             intent.putExtra("content", content)
-            startActivity(intent)
+
+            if(title.isNotEmpty() && content.isNotEmpty()) {
+                startActivity(intent)
+            }
         }//다음 액티비티로 넘어가는 버튼
 
-        binding.backButton.setOnClickListener{
-            finish()
+            binding.backButton.setOnClickListener {
+                finish()
+            }
         }
     }
-}
