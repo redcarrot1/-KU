5abc307a15956752e20e36a48b956013aca8a193
whyknow0604 <30621491+whyknow0604@users.noreply.github.com>
2023-06-02 16:00:21 +0900

    feat 조회 기능 UI 개발

---
+++ android/app/src/main/java/com/example/volunteerku/activity/VolunteerSearchActivity.kt
@@ -0,0 +1,12 @@
+package com.example.volunteerku.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.volunteerku.R
+
+class VolunteerSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_volunteer_search)
+    }
+}
