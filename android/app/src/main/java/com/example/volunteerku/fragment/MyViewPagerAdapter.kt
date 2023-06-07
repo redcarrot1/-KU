90f107e537f6a929aabadc7bb57f86be726d84e0
whyknow0604 <30621491+whyknow0604@users.noreply.github.com>
2023-06-07 20:34:13 +0900

    VolunteerSearch xml reqeust 성공

--- android/app/src/main/java/com/example/volunteerku/fragment/MyViewPagerAdapter.kt
+++ android/app/src/main/java/com/example/volunteerku/fragment/MyViewPagerAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.volunteerku.fragment
 
+import VolunteerSearch
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
--- android/app/src/main/java/com/example/volunteerku/fragment/VolunteerSearch.kt
+++ android/app/src/main/java/com/example/volunteerku/fragment/VolunteerSearch.kt
@@ -1,22 +1,63 @@
-package com.example.volunteerku.fragment
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.volunteerku.data.VolunteerData
 import com.example.volunteerku.databinding.ActivityVolunteerSearchBinding
+import okhttp3.*
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
+import java.io.IOException
+import java.io.InputStream
+import java.io.StringReader
 
 class VolunteerSearch : Fragment() {
 
     lateinit var binding: ActivityVolunteerSearchBinding
+    private val volunteerDataList: MutableList<VolunteerData> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = ActivityVolunteerSearchBinding.inflate(inflater, container, false)
+
+        volunteerSearch()
+
         return binding.root
     }
+
+    private fun volunteerSearch() {
+        val client = OkHttpClient()
+
+        val url = "http://openapi.1365.go.kr/openapi/service/rest/VolunteerPartcptnService/getVltrSearchWordList"
+
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // Handle request failure
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                    val responseData = response.body?.string()
+                    activity?.runOnUiThread {
+                        binding.textView11.text = responseData
+
+                    }
+                }
+            }
+        })
+    }
+
+
 
 }
